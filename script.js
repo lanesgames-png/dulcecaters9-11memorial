fa91a12afcd1ab896f4c567a3f5065124a7e82fb
lanesgames-png <laneancelet@gmail.com>
2025-12-17 20:23:20 -0600

    Update script.js

--- script.js
+++ script.js
@@ -139,6 +139,13 @@ searchBar.addEventListener("input", () => {
     }
   });
 });
+// script.js
+const cursor = document.querySelector(".cursor-glow");
+document.addEventListener("mousemove", e => {
+    cursor.style.left = e.clientX + "px";
+    cursor.style.top = e.clientY + "px";
+});
+
 
 
 
